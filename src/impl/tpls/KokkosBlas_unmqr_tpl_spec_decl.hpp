5f196acacb9137bccd5873a41f10b24aa358fa92
dialecticDolt <will.ruys@gmail.com>
2021-10-29 18:14:50 -0500

    Add unit test for GEQRF and UNMQR

--- src/impl/tpls/KokkosBlas_unmqr_tpl_spec_decl.hpp
+++ src/impl/tpls/KokkosBlas_unmqr_tpl_spec_decl.hpp
@@ -118,8 +118,9 @@ namespace KokkosBlas {
         bool C_is_lr = std::is_same<Kokkos::LayoutRight, LAYOUTC>::value; \
         const int AST = A_is_lr?A.stride(0):A.stride(1), LDA = AST == 0 ? 1:AST; \
         const int CST = C_is_lr?C.stride(0):C.stride(1), LDC = CST == 0 ? 1:CST; \
+        const char* complex_change = (trans == "T" || trans == "t") ? "C" : trans; \
         const int lwork = workspace.extent(0); \
-        HostLapack<S2>::unmqr(A_is_lr, side, trans, M, N, k, \
+        HostLapack<S2>::unmqr(A_is_lr, side, complex_change, M, N, k, \
                 reinterpret_cast<const S2*>(A.data()), LDA, \
                 reinterpret_cast<const S2*>(tau.data()), \
                 reinterpret_cast<S2*>(C.data()), LDC, \
@@ -165,7 +166,8 @@ namespace KokkosBlas {
         const int AST = A_is_lr?A.stride(0):A.stride(1), LDA = AST == 0 ? 1:AST; \
         const int CST = C_is_lr?C.stride(0):C.stride(1), LDC = CST == 0 ? 1:CST; \
         const int lwork = workspace.extent(0); \
-        HostLapack<S2>::unmqr(A_is_lr, side, trans, M, N, k, \
+        const char* complex_change = (trans == "T" || trans == "t") ? "C" : trans; \
+        HostLapack<S2>::unmqr(A_is_lr, side, complex_change, M, N, k, \
                 reinterpret_cast<const S2*>(A.data()), LDA, \
                 reinterpret_cast<const S2*>(tau.data()), \
                 reinterpret_cast<S2*>(C.data()), LDC, \
@@ -278,7 +280,7 @@ namespace KokkosBlas {
         bool C_is_lr = std::is_same<Kokkos::LayoutRight, LAYOUTC>::value; \
         const int AST = A_is_lr?A.stride(0):A.stride(1), LDA = AST == 0 ? 1:AST; \
         const int CST = C_is_lr?C.stride(0):C.stride(1), LDC = CST == 0 ? 1:CST; \
-        const char[] complex_change = (trans == "T" || trans == "t") ? "C" : trans; \
+        const char* complex_change = (trans == "T" || trans == "t") ? "C" : trans; \
         int lwork = -1; \
         SCALAR query = 0; \
         HostLapack<S2>::unmqr(A_is_lr, side, complex_change, M, N, k, \
@@ -322,7 +324,7 @@ namespace KokkosBlas {
         bool C_is_lr = std::is_same<Kokkos::LayoutRight, LAYOUTC>::value; \
         const int AST = A_is_lr?A.stride(0):A.stride(1), LDA = AST == 0 ? 1:AST; \
         const int CST = C_is_lr?C.stride(0):C.stride(1), LDC = CST == 0 ? 1:CST; \
-        const char[] complex_change = (trans == "T" || trans == "t") ? "C" : trans; \
+        const char* complex_change = (trans == "T" || trans == "t") ? "C" : trans; \
         int lwork = -1; \
         SCALAR query = 0; \
         HostLapack<S2>::unmqr(A_is_lr, side, complex_change, M, N, k, \
