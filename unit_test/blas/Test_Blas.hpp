6cf9478598d2e2b38789252c5202d910ad04fe8d
dialecticDolt <will.ruys@gmail.com>
2021-10-29 15:00:53 -0500

    Add unit test for GEQRF and UNMQR

--- unit_test/blas/Test_Blas.hpp
+++ unit_test/blas/Test_Blas.hpp
@@ -4,9 +4,8 @@
 #include "Test_Blas_gesv.hpp"
 #include "Test_Blas_trtri.hpp"
 
-//LAPACKE
-#include "Test_Blas_geqrf.hpp"
-#include "Test_Blas_unmqr.hpp"
+//LAPACK
+#include "Test_Blas_gr.hpp"
 
 // Blas 1
 #include "Test_Blas1_abs.hpp"
